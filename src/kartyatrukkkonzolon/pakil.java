a30fe00c3dec1a2a8959a7bc24afd75fd9a7c8fc
VorosDominik <medoxidhun@gmail.com>
2023-09-18 11:31:36 +0200

    rematch

---
+++ src/kartyatrukkkonzolon/pakil.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kartyatrukkkonzolon;
+
+/**
+ *
+ * @author voros.dominik
+ */
+class pakil {
+     private static String[] pakli = new String[22];
+
+    public pakil() {
+        
+    }
+    
+    
+     private  void feltolt() {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+        int i = 1;
+        for (String szin : szinek) {
+            for (int e = 0; e < ertekek.length && i < pakli.length; e++) {
+                pakli[i++] = szin + "_" + ertekek[e];
+            }
+        }
+
+    }
+    
+    private  void kirak() {
+        for (int i = 1; i < pakli.length; i++) {
+            System.out.printf("%-8s", pakli[i]);
+            if (i % 3 == 0) {
+                System.out.println("");
+            }
+        }
+    }
+
+
+
+    private int kever(int oszlop) {
+        // mindig középre a választott
+        String[] ujPakli = new String[22];
+        switch (oszlop) {
+            case 1:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 2:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 3:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[20 - (i - 1) * 3];
+                }
+                break;
+        }
+        pakli = ujPakli;
+         return oszlop;
+    }
+
+    private void ezVolt() {
+        System.out.println("A választott lap: " + pakli[11]);;
+    }
+}
